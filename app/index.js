3a8b3598b68ceba0f6c04cce2c07d3c0b49eede1
Ezechiele <ezechiele@MacBook-Pro.local>
2017-12-27 16:41:17 +0100

    Registration form

--- app/index.js
+++ app/index.js
@@ -8,9 +8,14 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
+app.use('/static', express.static('static'));
+
+
+app.get('/', (req, res) =>{
+  res.sendFile(__dirname + '/frontend/index.html');
 });
+
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
