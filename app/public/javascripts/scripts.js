87c69710a60fd3fe85eb87adaa414527ef448c75
Andrea Casarin <git@andreacasarin.com>
2017-12-29 16:13:19 +0100

    Fixing code style, cleaning up code.

--- app/public/javascripts/scripts.js
+++ app/public/javascripts/scripts.js
@@ -1,16 +1,14 @@
-$(document).ready(function(){
-    $("#get-users").click(function(){
-        $.ajax({
-                url: "http://localhost:3000/users",
-                success: function(data) {
-                    $("#get-users-listing").html('');
-                    for (var i = 0; i < data.length; i++) {
-                        element = data[i];
-                        $("#get-users-listing").append(
-                            '<p>' + element.email + ' -> ' + element.password + '</p>'
-                        );
-                    }
-                }
-        });
+$(document).ready(() => {
+  $('#get-users').click(() => {
+    $.ajax({
+      url: 'http://localhost:3000/users',
+      success: (data) => {
+        $('#get-users-listing').html('');
+        for (let i = 0; i < data.length; i += 1) {
+          const element = data[i];
+          $('#get-users-listing').append(`<p> ${element.email} -> ${element.password}</p>`);
+        }
+      },
     });
+  });
 });
