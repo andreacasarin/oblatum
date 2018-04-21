7aacb3ebcd2e9b5f13e4a31bfe7cd7b0c3308934
Andrea Casarin <git@andreacasarin.com>
2018-04-21 12:59:44 +0200

    Setting up for pilot.

--- app/controllers/misc.js
+++ app/controllers/misc.js
@@ -3,7 +3,7 @@ const mailer = require('../utils/mailer');
 exports.sendmail = (req, res) => {
   res.json(mailer.send({
     from: '"Oblatum 👻" <noreply@oblatum.it>',
-    to: 'reaalessandro95@gmail.com',
+    to: 'reaalessandro95@gmail.com, work@andreacasarin.com, support@oblatum.it',
     subject: 'Hello ✔',
     body: `Someone is interested in Oblatum: ${req.body.email}`,
   }));
