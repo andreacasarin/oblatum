651798ca2a97a921f7e1f8e76be11bc1cccb7b6b
Andrea Casarin <git@andreacasarin.com>
2018-09-10 15:34:10 +0200

    Removing database dependency from landing page version.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,20 +1,7 @@
 const express = require('express');
-const users = require('../controllers/users');
 const misc = require('../controllers/misc');
 
 const router = express.Router();
-
-/* Express. */
-// router.get('/', (req, res, next) => {
-//   res.render('index', { title: 'Express' });
-// });
-
-/* Users */
-// router.post('/users/:userId', users.create);
-// router.get('/users', users.read);
-// router.get('/users/:userId', users.read);
-// router.put('/users/:userId', users.update);
-// router.delete('/users/:userId', users.delete);
 
 /* Misc */
 router.post('/sendmail', misc.sendmail);
