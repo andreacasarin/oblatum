7a83a8469cb577283ce89c0739d97a1130685d89
Andrea Casarin <git@andreacasarin.com>
2018-12-06 18:42:38 +0100

    Updates pre-golive.

--- server/migrations/blockchain/2_deployment.js
+++ server/migrations/blockchain/2_deployment.js
@@ -1,4 +1,4 @@
-var DeedAuthority = artifacts.require("./DeedAuthority.sol");
+var DeedAuthority = artifacts.require("DeedAuthority");
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(DeedAuthority, { from: process.env.ETH_ADDRESS });
